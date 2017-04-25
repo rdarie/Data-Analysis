628577814c165d7a48ee56b6bc4e7f6f53db96e8
rdarie <radu_darie@brown.edu>
2017-04-25 18:30:34 -0400

    format confusion matrix

--- helper_functions.py
+++ helper_functions.py
@@ -628,7 +628,7 @@ def plotConfusionMatrix(cm, classes,
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
 
-    np.set_printoptions(precision=2)
+    #np.set_printoptions(precision=2)
     plt.title(title)
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
@@ -644,7 +644,7 @@ def plotConfusionMatrix(cm, classes,
 
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
+        plt.text(j, i, '{:.2f}'.format(cm[i, j]),
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
--- reload_plot.py
+++ reload_plot.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 
 localDir = 'Z:/data/rdarie/tempdata/Data-Analysis/'
 
 with open(localDir + 'spikeConfusionMatrix.pickle', 'rb') as f:
     ax = pickle.load(f)
 
+#np.set_printoptions(precision=2)
 plt.show()
