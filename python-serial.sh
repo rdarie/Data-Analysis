65c935a21504e87ed1cd127934acfde86940d407
Radu Darie <rdarie@login001.oscar.ccv.brown.edu>
2017-04-27 16:35:22 -0400

    added ccv batch job

---
+++ python-serial.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Request an hour of runtime:
+#SBATCH --time=0:10:00
+
+# Default resources are 1 core with 2.8GB of memory.
+
+# Use more memory (4GB):
+#SBATCH --mem=64G
+#SBATCH -n 12
+
+# Specify a job name:
+#SBATCH -J PythonJob
+
+# Run a matlab function called 'foo.m' in the same directory as this batch script.
+python mainSpikes.py
