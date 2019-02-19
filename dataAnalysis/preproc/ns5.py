0d6b306e5947bf2659a3edc44861538472a265b5
Radu Darie <radu_darie@brown.edu>
2019-02-19 09:48:04 -0500

    Update ns5.py

--- dataAnalysis/preproc/ns5.py
+++ dataAnalysis/preproc/ns5.py
@@ -28,44 +28,39 @@ try:
 except ImportError:
     from collections import Iterable
 
+
 def getNSxData(
-        filePath, elecIds, startTime_s,
-        dataLength_s, spikeStruct, downsample=1):
+        fileName=None,
+        folderPath=None,
+        reader=None, elecIds=None, startTime_s=None,
+        dataLength_s=None, downsample=1):
 
-    elecCorrespondence = spikeStruct.loc[elecIds, 'nevID']
-    elecCorrespondence = elecCorrespondence.astype(int)
     #  Open file and extract headers
-    reader = neo.io.BlackrockIO(filename=filePath)
-
-    nsx_file = NsxFile(filePath)
-    #  Extract data - note: data will be returned based on *SORTED* nevIds, see cont_data['elec_ids']
+    if reader is None:
+        assert (fileName is not None) and (folderPath is not None)
+        filePath = os.path.join(folderPath, fileName) + '.nix'
+        reader = neo.io.nixio_fr.NixIO(filename=filePath)
+    
+    idx = 0
+    reader.parse_header()
+    metadata = reader.header
     #  pdb.set_trace()
-    channelData = nsx_file.getdata(
-        list(elecCorrespondence.values), startTime_s, dataLength_s, downsample)
-
-    rowIndex = range(
-        int(channelData['start_time_s'] * channelData['samp_per_s']),
-        int((channelData['start_time_s'] + channelData['data_time_s']) *
-            channelData['samp_per_s']))
-    channelData['data'] = pd.DataFrame(
-        channelData['data'].transpose(),
-        index=rowIndex, columns=elecCorrespondence.sort_values().index.values)
-
-    channelData['t'] = channelData['start_time_s'] + np.arange(channelData['data'].shape[0]) / channelData['samp_per_s']
-    channelData['t'] = pd.Series(
-        channelData['t'], index=channelData['data'].index)
-    channelData['badData'] = {}
-    channelData['basic_headers'] = nsx_file.basic_header
-    channelData['extended_headers'] = nsx_file.extended_headers
-    # Close the nsx file now that all data is out
-    nsx_file.close()
+    for blkIdx in range(metadata['nb_block']):
+        #  blkIdx = 0      
+        blockMeta = reader.read_block(block_index=blkIdx, lazy=True)  
+        for segIdx in range(metadata['nb_segment'][blkIdx]):
+            #  segIdx = 0
+            nSamp = reader.get_signal_size(blkIdx, segIdx)
+            blockMeta = reader.read_block(block_index=blkIdx, lazy=True)
+            segMeta = blockMeta.segments[segIdx]
+            print(segMeta.t_start)
+    channelData = None
     return channelData
 
 
 def preproc(
         fileName='Trial001',
         folderPath='./',
-        elecIds=range(1, 97), startTimeS=0, dataTimeS='all',
         fillOverflow=True, removeJumps=True,
         chunkSize=1800
         ):
@@ -84,13 +79,19 @@ def preproc(
     idx = 0
     for blkIdx in range(metadata['nb_block']):
         #  blkIdx = 0
-        #  load block metadata only
-        #  blockMeta = reader.read_block(block_index=blkIdx, lazy=True)
-        #  make a copy and strip it of placeholders
-        #  blockMeta.segments = []
-        #  blockMeta.channel_indexes = []
-        #  writer.write_block(blockMeta)
-        
+        blockMeta = reader.read_block(block_index=blkIdx, lazy=True)
+        #  prune out nev spike placeholders
+        for metaIdx, chanIdx in enumerate(blockMeta.channel_indexes):
+            if chanIdx.units:
+                for unitIdx, unit in enumerate(chanIdx.units):
+                    print(unit.spiketrains)
+                    unit.spiketrains = []
+                chanIdx.units = []
+                #  chanIdx.name += ' ' + chanIdx.units[0].name
+        blockMeta.channel_indexes = (
+            [chanIdx for chanIdx in blockMeta.channel_indexes if (
+                chanIdx.analogsignals)])
+
         for segIdx in range(metadata['nb_segment'][blkIdx]):
             #  segIdx = 0
             nSamp = reader.get_signal_size(blkIdx, segIdx)
@@ -103,7 +104,9 @@ def preproc(
 
                 blockMeta = reader.read_block(block_index=blkIdx, lazy=True)
                 segMeta = blockMeta.segments[segIdx]
-                segMeta.index = idx
+                for metaIdx, chanIdx in enumerate(blockMeta.channel_indexes):
+                    if 'Unit' in chanIdx.name:
+                        blockMeta.channel_indexes[metaIdx].name += '{}'.format(metaIdx)
                 #  see https://neo.readthedocs.io/en/latest/_images/simple_generated_diagram.png
                 #  seg is a new object in memory, swap it out
                 #  for the metadata placeholder
@@ -115,8 +118,25 @@ def preproc(
                     i_start=i_start, i_stop=i_stop)
                 # recalc i_stop in case there wasn't enough data
                 i_stop = i_start + chunkData.shape[0]
+                # actualChunkDur = chunkData.shape[0] / fs 
                 #  actually do the preprocessing on the chunkData
-                
+                if fillOverflow:
+                    # fill in overflow:
+                    pass
+                    '''
+                    timeSection['data'], overflowMask = hf.fillInOverflow(
+                        timeSection['data'], fillMethod = 'average')
+                    #  todo: add epoch showing problematic period
+                    '''
+                if removeJumps:
+                    # find unusual jumps in derivative or amplitude
+                    pass
+                    '''
+                    timeSection['data'], newBadData = hf.fillInJumps(timeSection['data'],
+                        timeSection['samp_per_s'], smoothing_ms = 0.5, nStdDiff = 50,
+                        nStdAmp = 100)
+                    #  todo: add epoch showing problematic period
+                    '''
                 segMeta.spiketrains = []
                 segMeta.epochs = []
                 segMeta.events = []
@@ -124,8 +144,9 @@ def preproc(
 
                 #  replace analogsignal proxies with the chunk data
                 for aIdx, aSigProxy in enumerate(segMeta.analogsignals):
+                    chanIdx = aSigProxy.channel_index
                     aSig = AnalogSignal(
-                        chunkData[:, aSigProxy.channel_index.index],
+                        chunkData[:, chanIdx.index],
                         units=aSigProxy.units, dtype=aSigProxy.dtype,
                         t_start=aSigProxy.t_start + curChunk * chunkSize * s,
                         sampling_rate=aSigProxy.sampling_rate,
@@ -134,75 +155,35 @@ def preproc(
                         description=aSigProxy.description,
                         array_annotations=aSigProxy.array_annotations,
                         **aSigProxy.annotations)
-                    #  fix the channel index references
-                    aSigProxy.channel_index = aSig.channel_index
+
+                    #  fix the channel index pointers
+                    assert len(chanIdx.analogsignals) == 1
+                    chanIdx.analogsignals[0] = aSig
+                    #  purge the spiketrains
+                    for unit in chanIdx.units:
+                        unit.spiketrains = []
+                    chanIdx.units = []
                     #  save the data
                     segMeta.analogsignals[aIdx] = aSig
+                
+                # purge units on remaining channels
+                for chanIdx in blockMeta.channel_indexes:
+                    for unit in chanIdx.units:
+                        unit.spiketrains = []
+                        #  print(chanIdx.channel_names)
+                    chanIdx.units = []
+                
+                segMeta.index = idx
+                blockMeta.index = idx
                 idx += 1
-                writer.write_block(blockMeta)
-    return writer
+                writer.write_block(blockMeta, use_obj_names=True)
 
-def preprocSection(
-    fileName='Trial001',
-    folderPath = './',
-    elecIds = range(1, 97), startTimeS = 0, dataTimeS = 900,
-    chunkSize = 900,
-    curSection = 0, sectionsTotal = 1,
-    fillOverflow = False, removeJumps = True):
-
-    filePath     = os.path.join(folderPath, fileName + '.h5')
-    timeSection  = hf.getNSxData(filePath, elecIds, startTimeS, dataTimeS)
-
-    origDataPath = os.path.join(folderPath, 'dataAnalysisPreproc',
-        fileName + '_orig_%dof%d.npy' % (curSection + 1, sectionsTotal))
-    np.save(origDataPath, timeSection['data'].values)
-    badData = {}
-
-    if fillOverflow:
-        # fill in overflow:
-        timeSection['data'], overflowMask = hf.fillInOverflow(
-            timeSection['data'], fillMethod = 'average')
-        badData.update({'overflow': overflowMask})
-
-    if removeJumps:
-        # find unusual jumps in derivative or amplitude
-        timeSection['data'], newBadData = hf.fillInJumps(timeSection['data'],
-            timeSection['samp_per_s'], smoothing_ms = 0.5, nStdDiff = 50,
-            nStdAmp = 100)
-        badData.update(newBadData)
-
-    timeSection['badData'] = badData
-
-    print('Saving clean data')
-    #
-    dataPath = os.path.join(folderPath, 'dataAnalysisPreproc',
-        fileName + '_clean.h5')
-    with h5py.File(dataPath, "a") as f:
-        #  print(timeSection['data'])
-        #  pdb.set_trace()
-        sectionIndex = slice(
-            int(curSection * chunkSize * timeSection['samp_per_s']),
-            int((curSection * chunkSize + dataTimeS) * timeSection['samp_per_s'])
-            )
-        f['data'][sectionIndex, :] = timeSection['data'].values
-        f['index'][sectionIndex] = timeSection['data'].index
-        f['t'][sectionIndex] = timeSection['t'].values
-
-    del timeSection['data'], timeSection['t']
-
-    metaDataPath = os.path.join(folderPath, 'dataAnalysisPreproc',
-        fileName + '_clean_metadata_%dof%d.pickle' % (curSection + 1, sectionsTotal))
-
-    with open(metaDataPath, "wb" ) as f:
-        pickle.dump(timeSection, f, protocol=4 )
-
-    print('Done cleaning data')
-    return section
-
-def preprocNs5Spectrum(stepLen_s = 0.05, winLen_s = 0.1,
-    fr_start = 5, fr_stop = 1000):
-    pass
-
+    writer.close()
+    channelData = getNSxData(
+        reader=neo.io.nixio_fr.NixIO(filename=trialBasePath + '.nix'),
+        elecIds=None, startTime_s=None,
+        dataLength_s=None, downsample=1)
+    return channelData
 
 def loadTimeSeries(
         filePath=None, elecIds=None,
