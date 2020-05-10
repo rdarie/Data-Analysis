b912cdf552fecb809ab0d080e0211c9ae7e29740
Radu Darie <radu_darie@brown.edu>
2020-05-10 17:31:15 -0400

    bumping version for standardized requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('requirements.txt') as fp:
     install_requires = fp.read()
 setup(
     name='ProprioDataAnalysis',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires=install_requires,
