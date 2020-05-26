e8ad5808b15a4e1c5d7468f768e4faf38f97316f
Radu Darie <radu_darie@brown.edu>
2020-05-26 19:15:29 -0400

    ...

--- install_windows.sh
+++ install_windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GitRepoRoot="git://github.com/rdarie/"
-#GitRepoRoot="git://github.com/neuromotion/"
+
 RepoList=(\
 "seaborn" \
 "python-neo" \
@@ -18,22 +18,23 @@ RepoList=(\
 # conda activate nda2
 # cd ..
 #
-pip install pyqt5==5.10.1 --user
-pip install vg==1.6.1 --user
-pip install mpi4py==3.0.3 --user
-pip install git+git://github.com/G-Node/nixpy@v1.5.0b3 --user
-pip install git+git://github.com/hector-sab/ttictoc@v0.4.1 --user
-pip install git+git://github.com/raphaelvallat/pingouin@v0.3.3 --user
+# pip install pyqt5==5.10.1 --user
+# pip install vg==1.6.1 --user
+# pip install mpi4py==3.0.3 --user
+# pip install git+git://github.com/G-Node/nixpy@v1.5.0b3 --user
+# pip install git+git://github.com/hector-sab/ttictoc@v0.4.1 --user
+# pip install git+git://github.com/raphaelvallat/pingouin@v0.3.3 --user
 #
 
-for i in ${RepoList[*]}; do
-    echo $GitRepoRoot$i".git"
-    git clone $GitRepoRoot$i".git"
-    cd $i
-    # git checkout tags/ndav0.2
-    python setup.py develop --user
-    cd ..
-done
-#
-cd Data-Analysis
-python setup.py develop --user
+# for i in ${RepoList[*]}; do
+#     echo $GitRepoRoot$i".git"
+#     git clone $GitRepoRoot$i".git"
+#     cd $i
+#     # git checkout tags/ndav0.2
+#     python setup.py develop --user
+#     cd ..
+# done
+# #
+# cd Data-Analysis
+# python setup.py develop --user
+# 
