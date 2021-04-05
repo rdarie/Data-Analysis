cea24d354502a5ae0bea0bc060fffa2239368ea0
Radu Darie <radu_darie@brown.edu>
2021-04-05 11:56:17 -0400

    Update run_pca_dimensionality_aligned_stim_kcsd.sh

--- dataAnalysis/analysis-code/shellScripts/run_pca_dimensionality_aligned_stim_kcsd.sh
+++ dataAnalysis/analysis-code/shellScripts/run_pca_dimensionality_aligned_stim_kcsd.sh
@@ -25,11 +25,11 @@
 SLURM_ARRAY_TASK_ID=2
 source ./shellScripts/run_pca_calc_aligned_stim_preamble.sh
 
-python -u './calcLaplacianFromTriggered.py' --plotting --recalcKCSDCV --useKCSD --inputBlockSuffix="lfp" --unitQuery="lfp" --outputBlockSuffix="kcsd_triggered" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR $OUTLIERMASK $LAZINESS
+# python -u './calcLaplacianFromTriggered.py' --plotting --recalcKCSDCV --useKCSD --inputBlockSuffix="lfp" --unitQuery="lfp" --outputBlockSuffix="kcsd_triggered" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR $OUTLIERMASK $LAZINESS
 
 # python -u './calcPCAinChunks.py' --inputBlockSuffix="kcsd" --unitQuery="lfp" --estimatorName="pca_lapl" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR $OUTLIERMASK
 python -u './calcSignalDimensionality.py' --inputBlockSuffix="kcsd" --unitQuery="lfp" --estimatorName="pca_lapl" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR
-python -u './calcSignalDimensionality.py' --inputBlockSuffix="lfp_CAR" --unitQuery="lfp" --estimatorName="pca_car" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR
+# python -u './calcSignalDimensionality.py' --inputBlockSuffix="lfp_CAR" --unitQuery="lfp" --estimatorName="pca_car" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR
 # python -u './calcSparsePCA.py' --inputBlockSuffix="kcsd" --unitQuery="lfp" --estimatorName="sparse_pca_lapl" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR $OUTLIERMASK
 # python -u './applyEstimatorToTriggered.py' --inputBlockSuffix="kcsd" --estimatorName="pca_lapl" --unitQuery="lfp" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR
 # python -u './calcSpectralFeatures.py' --inputBlockSuffix="kcsd_pca" --unitQuery="pca" --exp=$EXP $WINDOW $ALIGNQUERY $ANALYSISFOLDER $ALIGNFOLDER $BLOCKSELECTOR
