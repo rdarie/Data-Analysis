baf4c3033b47ccf339e3b19e59f64f23e9797cd8
Radu Darie <radu_darie@brown.edu>
2021-05-22 10:51:16 -0400

    ..

---
+++ dataAnalysis/analysis_code/shellScripts/run_aligned_nix_assembler_motion.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Request runtime:
+#SBATCH --time=12:00:00
+
+# Default resources are 1 core with 2.8GB of memory.
+
+# Request memory:
+#SBATCH --nodes=1
+#SBATCH --mem=127G
+
+# Specify a job name:
+#SBATCH -J test_train_split
+
+# Specify an output file
+#SBATCH -o ../../batch_logs/%j-%a-test_train_split.out
+#SBATCH -e ../../batch_logs/%j-%a-test_train_split.out
+
+# Specify account details
+#SBATCH --account=carney-dborton-condo
+
+# Request custom resources
+#               SBATCH --array=2,3
+
+SLURM_ARRAY_TASK_ID=2
+source ./shellScripts/calc_aligned_motion_preamble.sh
+
+BLOCKSELECTOR="--blockIdx=${SLURM_ARRAY_TASK_ID} --processAll"
+TARGET="pedalState"
+
+blocks=(lfp_CAR rig)
+# blocks=(lfp_CAR lfp_CAR_spectral rig)
+for B in "${blocks[@]}"
+do
+    echo "concatenating $B blocks"
+    python -u ./assembleExperimentAlignedAsigs.py --exp=$EXP $BLOCKSELECTOR --inputBlockSuffix=$B $WINDOW $ANALYSISFOLDER $ALIGNFOLDER $LAZINESS
+done
